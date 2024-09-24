5c51edbe233b4514c55a1db973b4a908b9d3781d
DevDrache <dominik.mast.11@gmail.com>
2024-09-24 14:01:33 +0300

    clean code

--- main_menu_page/main_menu.gd
+++ main_menu_page/main_menu.gd
@@ -2,15 +2,19 @@ extends Control
 
 @export var initalPage = 0
 
+@onready var buttons: HBoxContainer = %Buttons
+@onready var page_container: Control = %PageContainer
+
 const WORKOUT_A = preload("res://resrouces/workout_resources/Workout.tres")
 
 func _ready():
-	%WorkoutButton.pressed.connect(_open_page.bind(0))
-	%SkillTreeButton.pressed.connect(_open_page.bind(1))
-	%StatisticButton.pressed.connect(_open_page.bind(2))
-	
 	_open_page(initalPage)
 	
+	for button in buttons.get_children():
+		button.pressed.connect(_open_page.bind(button.get_index()))
+		button.pressed.connect(_open_page.bind(button.get_index()))
+		button.pressed.connect(_open_page.bind(button.get_index()))
+	
 func _open_page(pageNumber):
-	for i in %PageContainer.get_child_count():
-		%PageContainer.get_children()[i].visible = i == pageNumber
+	for i in page_container.get_child_count():
+		page_container.get_children()[i].visible = i == pageNumber
