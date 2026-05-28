f1892b584176c09ddae17980a9f95e8dbf4b07b6
DevDrache <dominik.mast.11@gmail.com>
2026-05-28 07:42:44 +0300

    add calender selected date color

--- widgets/calendar/date_label.gd
+++ widgets/calendar/date_label.gd
@@ -8,26 +8,49 @@ class_name DateLabel
 @export var date: Dictionary
 @export var workoutData: WorkoutResource
 
-const WORKOUT_STILL_DO_COLOR = Color(0.5,0.5,0.5,1)
+const CURRENT_DATE_COLOR = Color("2161bd")
+const DATE_SELECT_COLOR = Color(0.5,0.5,0.5,1)
+const WORKOUT_STILL_DO_COLOR = Color(1.0, 1.0, 1.0, 1.0)
 const WORKOUT_DONE_COLOR = Color(0,0.5,0,1)
 const WORKOUT_NOT_MADE_COLOR = Color(1.0, 0.0, 0.0, 1.0)
 
 func _ready() -> void:
+	SignalHub.calendar_date_selected.connect(_on_date_selected)
+	
 	date_label.text = str(date.day)
 	
 	var currentDate := Time.get_datetime_dict_from_system()
 	var isCurrentDate: bool = date.day == currentDate.day and date.month == currentDate.month and date.year == currentDate.year
 	
 	if not isCurrentDate: 
-		panel.add_theme_stylebox_override("panel", StyleBoxEmpty.new())
+		_set_panel_color(Color.TRANSPARENT)
 	
 	if not workoutData: 
 		workout_indicator.add_theme_stylebox_override("panel", StyleBoxEmpty.new())
 	else: 
 		_set_workout_indicator()
 
+func _on_date_selected(selectedDate: Dictionary) -> void:
+	if selectedDate == date:
+		return
+
+	var currentDate := Time.get_datetime_dict_from_system()
+	var isCurrentDate: bool = date.day == currentDate.day and date.month == currentDate.month and date.year == currentDate.year
+	if isCurrentDate:
+		_set_panel_color(CURRENT_DATE_COLOR)
+	else:
+		_set_panel_color(Color.TRANSPARENT)
+
+
 func _on_button_pressed() -> void:
+	_set_panel_color(DATE_SELECT_COLOR)
+	
 	SignalHub.calendar_date_selected.emit(date)
+
+func _set_panel_color(color: Color) -> void:
+	var styleBox :StyleBoxFlat = panel.get_theme_stylebox("panel").duplicate()
+	styleBox.bg_color = color
+	panel.add_theme_stylebox_override("panel", styleBox)
 
 func _set_workout_indicator() -> void:
 	var styleBox :StyleBoxFlat = workout_indicator.get_theme_stylebox("panel").duplicate()
